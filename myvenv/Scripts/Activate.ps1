0edb58ff0377293b6ace77d56f35cca332f1703f
Ruben <rcarralafuente@gmail.com>
2019-03-30 14:08:58 -0600

    Añadidas vistas

--- myvenv/Scripts/Activate.ps1
+++ myvenv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\siga\myvenv"
+$env:VIRTUAL_ENV="C:\Siga\myvenv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
